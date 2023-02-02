b10dedba762d9cef876c73c9605c24362da6af47
yonilurie <yonilurie@protonmail.com>
2023-02-01 20:10:21 -0800

    Redid structure, created navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,64 @@
-import { useState } from "react";
 import "./App.css";
-import { Drawer } from "@mui/material";
+import { useState } from "react";
 function App() {
-	const [page, setPage] = useState("home");
-	const [showDrawer, setShowDrawer] = useState(false);
+	const [showContact, setShowContact] = useState(false);
+
+	const toggleContact = () => {
+		if (showContact) {
+			let contact = document.getElementById("contact");
+			contact?.classList.add("fade-out");
+			let contactContainer = document.getElementById("contact-container");
+			contactContainer?.classList.add("fade-out");
+			const timeout = setTimeout(() => {
+				setShowContact((show) => !show);
+			}, 500);
+			return () => clearTimeout(timeout);
+		} else {
+			setShowContact((show) => !show);
+		}
+	};
+
 	return (
 		<div className="App">
-			<Drawer
-				open={showDrawer}
-				anchor={"right"}
-				hideBackdrop={false}
-				onClose={() => setShowDrawer(false)}
-			>
-				<div className="tabs-container">
-					<button>Home</button>
-					<button>About</button>
-					<button>Projects</button>
-					<button>Games</button>
+			{showContact && <Contact toggleContact={toggleContact} />}
+			<nav>
+				<div>
+					<a
+						href="/src/assets/YonatanLurieResume.pdf"
+						target="_blank"
+						referrerPolicy="no-referrer"
+					>
+						resume
+					</a>
 				</div>
-			</Drawer>
-			<h1>Yonatan Lurie</h1>
-			<h2>Full Stack Software Engineer</h2>
-			<button
-				className="drawer-toggle"
-				onClick={() => setShowDrawer((drawer) => !drawer)}
-			>
-				<span className="material-icons">menu</span>
-			</button>
+				<button onClick={toggleContact}>contact me</button>
+			</nav>
+			<section className="who-am-i">
+				<h1>yonatan lurie</h1>
+				<h2>full stack software engineer</h2>
+			</section>
 		</div>
 	);
 }
 
 export default App;
+
+function Contact({ toggleContact }) {
+	return (
+		<div className="contact-container" id="contact-container">
+			<div className="contact" id="contact">
+				<button className="exit-contact" onClick={toggleContact}>
+					X
+				</button>
+				<h2>Contact me</h2>
+				<form action="https://formspree.io/f/xgeqapga" method="POST">
+					<label htmlFor="email">Your email:</label>
+					<input type="email" name="email" id="email" required />
+					<label htmlFor="message">Your message:</label>
+					<textarea name="message" id="message" required></textarea>
+					<button type="submit">Send</button>
+				</form>
+			</div>
+		</div>
+	);
+}
