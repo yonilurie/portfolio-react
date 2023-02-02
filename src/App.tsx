284cf341ec26969908ead9a8a9d642de92b38a02
yonilurie <yonilurie@protonmail.com>
2023-02-01 18:25:16 -0800

    Added drawer menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
+import { useState } from "react";
+import "./App.css";
+import { Drawer } from "@mui/material";
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+	const [page, setPage] = useState("home");
+	const [showDrawer, setShowDrawer] = useState(false);
+	return (
+		<div className="App">
+			<Drawer
+				open={showDrawer}
+				anchor={"right"}
+				hideBackdrop={false}
+				onClose={() => setShowDrawer(false)}
+			>
+				<div className="tabs-container">
+					<button>Home</button>
+					<button>About</button>
+					<button>Projects</button>
+					<button>Games</button>
+				</div>
+			</Drawer>
+			<h1>Yonatan Lurie</h1>
+			<h2>Full Stack Software Engineer</h2>
+			<button
+				className="drawer-toggle"
+				onClick={() => setShowDrawer((drawer) => !drawer)}
+			>
+				<span className="material-icons">menu</span>
+			</button>
+		</div>
+	);
 }
 
-export default App
+export default App;
