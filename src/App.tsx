5ea5231e30a1f9e41752fe91c74a56edfc5388ee
yonilurie <yonilurie@protonmail.com>
2023-02-04 14:31:19 -0800

    added projects info

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,20 @@ function App() {
 			{showContact && <Contact toggleContact={toggleContact} />}
 			<nav>
 				<a
+					href="https://github.com/yonilurie"
+					target="_blank"
+					referrerPolicy="no-referrer"
+				>
+					<i className="fa-brands fa-github"></i>
+				</a>
+				<a
+					href="https://www.linkedin.com/in/yonilurie/"
+					target="_blank"
+					referrerPolicy="no-referrer"
+				>
+					<i className="fa-brands fa-linkedin-in"></i>
+				</a>
+				<a
 					href="/src/assets/YonatanLurieResume.pdf"
 					target="_blank"
 					referrerPolicy="no-referrer"
@@ -85,56 +99,261 @@ function App() {
 				</div>
 			</section>
 			<section className="projects">
-				<h3>projects</h3>
+				<h3>Projects</h3>
 				<div className="single-project">
-					<p>Name</p>
-					<div>
-						<img />
-						<div>Tools</div>
-						<div>description</div>
+					<div className="single-project-info">
+						<h4>Instagram Clone</h4>
+						<div className="project-links">
+							<a
+								href="https://thegramme.herokuapp.com/"
+								target="_blank"
+								referrerPolicy="no-referrer"
+							>
+								Live Site
+							</a>
+							<a
+								href="https://github.com/yonilurie/instagram-clone"
+								target="_blank"
+								referrerPolicy="no-referrer"
+							>
+								GitHub
+							</a>
+						</div>
+						<code>
+							(JavaScript, Python, React, Redux, Flask,
+							SQLAlchemy, CSS)
+						</code>
+						<p>
+							Features include, user authentication, creating and
+							editing posts, creating and editing comments, liking
+							posts as well as comments, and following other
+							users.
+						</p>
 					</div>
+
+					<a
+						href="https://thegramme.herokuapp.com/"
+						target="_blank"
+						referrerPolicy="no-referrer"
+						className="project-img"
+					>
+						<img src="/src/assets/thegramme-img.png"></img>
+					</a>
 				</div>
 				<div className="single-project">
-					<p>Name</p>
-					<div>
-						<img />
-						<div>Tools</div>
-						<div>description</div>
+					<div className="single-project-info">
+						<h4>Reddit Clone</h4>
+						<div className="project-links">
+							<a
+								href="https://teddir.herokuapp.com/"
+								target="_blank"
+								referrerPolicy="no-referrer"
+							>
+								Live Site
+							</a>
+							<a
+								href="https://github.com/yonilurie/reddit-clone"
+								target="_blank"
+								referrerPolicy="no-referrer"
+							>
+								GitHub
+							</a>
+						</div>
+						<code>
+							(JavaScript, Python, React, Redux, Flask,
+							SQLAlchemy, CSS)
+						</code>
+						<p>
+							Features include, user authentication, creating and
+							editing posts, creating and editing comments, nested
+							comments, liking posts as well as comments, creating
+							a subreddit along with subreddit customization, and
+							a search feature.
+						</p>
+						<p>All images are hosted using AWS.</p>
 					</div>
+
+					<a
+						href="https://teddir.herokuapp.com/"
+						target="_blank"
+						referrerPolicy="no-referrer"
+						className="project-img"
+					>
+						<img src="/src/assets/teddir-img.png"></img>
+					</a>
 				</div>
 				<div className="single-project">
-					<p>Name</p>
-					<div>
-						<img />
-						<div>Tools</div>
-						<div>description</div>
+					<div className="single-project-info">
+						<h4>Airbnb Clone</h4>
+						<div className="project-links">
+							<a
+								href="https://a-bnb.herokuapp.com/"
+								target="_blank"
+								referrerPolicy="no-referrer"
+							>
+								Live Site
+							</a>
+							<a
+								href="https://github.com/yonilurie/airbnb-clone"
+								target="_blank"
+								referrerPolicy="no-referrer"
+							>
+								GitHub
+							</a>
+						</div>
+						<p>
+							<code>
+								(JavaScript, React, Redux, Express, Sequelize,
+								CSS)
+							</code>
+						</p>
+						<p>
+							Features include, user authentication, creating and
+							editing rooms, creating and editing reviews, and
+							booking a stay at other users places.
+						</p>
+						<p>
+							Additionally, the Google Maps API is used to show
+							exactly where a room is on the map. All images are
+							also hosted using AWS.
+						</p>
 					</div>
+					<a
+						href="https://a-bnb.herokuapp.com/"
+						target="_blank"
+						referrerPolicy="no-referrer"
+						className="project-img"
+					>
+						<img src="/src/assets/abnb-img.png"></img>
+					</a>
 				</div>
 				<div className="single-project">
-					<p>Name</p>
-					<div>
-						<img />
-						<div>Tools</div>
-						<div>description</div>
+					<div className="single-project-info">
+						<h4>Wordle</h4>
+						<div className="project-links">
+							<a
+								href="https://nyt-wordle-clone.netlify.app/"
+								target="_blank"
+								referrerPolicy="no-referrer"
+							>
+								Live Site
+							</a>
+							<a
+								href="https://github.com/yonilurie/Wordle"
+								target="_blank"
+								referrerPolicy="no-referrer"
+							>
+								GitHub
+							</a>
+						</div>
+						<p>
+							<code>(TypeScript, React, CSS)</code>
+						</p>
+						<p>
+							This app is a clone of the New York Times Wordle
+							game. A word is picked at random and the user has 6
+							attempts to guess it correctly.
+						</p>
+						<p>
+							Each guess has the opportunity to reveal
+							information. Each letter in the users guess that is
+							in the word will be colored in gold. Each letter
+							that is in the same place as the word will be green.
+							All other letters will be grey.
+						</p>
 					</div>
+					<a
+						href="https://nyt-wordle-clone.netlify.app/"
+						target="_blank"
+						referrerPolicy="no-referrer"
+						className="project-img"
+					>
+						<img src="/src/assets/wordle-img.png"></img>
+					</a>
 				</div>
 				<div className="single-project">
-					<p>Name</p>
-					<div>
-						<img />
-						<div>Tools</div>
-						<div>description</div>
+					<div className="single-project-info">
+						<h4>Boggle</h4>
+						<div className="project-links">
+							<a
+								href="https://boggle-yonilurie.netlify.app/"
+								target="_blank"
+								referrerPolicy="no-referrer"
+							>
+								Live Site
+							</a>
+							<a
+								href="https://github.com/yonilurie/Boggle"
+								target="_blank"
+								referrerPolicy="no-referrer"
+							>
+								GitHub
+							</a>
+						</div>
+						<p>
+							<code>(TypeScript, React, CSS)</code>
+						</p>
+						<p>The game of Boggle.</p>
+						<p>
+							A 4X4 board of letters is generated, the user has 3
+							minutes to find as many words comprised of adjacent
+							cells as possible.
+						</p>
 					</div>
+
+					<a
+						href="https://boggle-yonilurie.netlify.app/"
+						target="_blank"
+						referrerPolicy="no-referrer"
+						className="project-img"
+					>
+						<img src="/src/assets/boggle-img.png"></img>
+					</a>
 				</div>
 				<div className="single-project">
-					<p>Name</p>
-					<div>
-						<img />
-						<div>Tools</div>
-						<div>description</div>
+					<div className="single-project-info">
+						<h4>Etch-A-Sketch</h4>
+						<div className="project-links">
+							<a
+								href="https://inspiring-gumption-0c3a95.netlify.app/"
+								target="_blank"
+								referrerPolicy="no-referrer"
+							>
+								Live Site
+							</a>
+							<a
+								href="https://yonilurie.github.io/Etch-A-Sketch-Web/"
+								target="_blank"
+								referrerPolicy="no-referrer"
+							>
+								GitHub
+							</a>
+						</div>
+						<p>
+							<code>(JavaScript, HTML, CSS)</code>
+						</p>
+						<p>
+							Etch-A-Sketch is supposed to simulate an
+							Etch-A-Sketch. Slide your mouse across the canvas to
+							paint whatever you like.
+						</p>
+						<p>
+							Features include rainbow mode, a color picker,
+							eraser, and an option to resize the canvas.
+						</p>
 					</div>
+
+					<a
+						href="https://inspiring-gumption-0c3a95.netlify.app/"
+						target="_blank"
+						referrerPolicy="no-referrer"
+						className="project-img"
+					>
+						<img src="/src/assets/etch-img.png"></img>
+					</a>
 				</div>
 			</section>
+			<footer></footer>
 		</div>
 	);
 }
